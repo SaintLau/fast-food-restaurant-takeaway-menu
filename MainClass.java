b36a512d09446af9a00d4ad3d06390ffa7709df0
laura.santos <laura-santos@future-healthcare.eu>
2022-02-25 12:49:04 +0000

    start build main class

--- MainClass.java
+++ MainClass.java
@@ -1,1 +1,31 @@
 //THE CLASS
+public class MainClass {
+    //what do we need from the person -> name, contact info, food, 
+    private String FullName;
+    private int ContactNumber;
+    private double Full_Price;
+
+    public void setFullName(String fullName) {
+        this.FullName = fullName;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setContactNumber(int contactNumber) {
+        this.ContactNumber = contactNumber;
+    }
+
+    public int getContactNumber() {
+        return ContactNumber;
+    }
+
+    public void setFullPrice(double fullPrice) {
+        this.Full_Price = fullPrice;
+    }
+
+    public double getFullPrice() {
+        return Full_Price;
+    }
+}

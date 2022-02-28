9f791d183deb3775fcc028dca1b157f194d2c2f2
laura.santos <laura-santos@future-healthcare.eu>
2022-02-28 11:16:50 +0000

    add main ingredients

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+package src;
+
+//imports
+import javax.swing.*;
+
+
+public class Main {
+    public static MainClass mc = new MainClass();
+
+    public static String full_Name;
+    public static String contactNumber;
+    public static int $food_choice, $drink_choice, $side_choice;
+    public static double $price;
+    public static double $total;
+
+    // Menu prices
+
+    //Food
+    public static double $burger = 5.45;
+    public static double $pasta = 11.65;
+    public static double $taco = 7.75;
+
+    //Drinks
+    public static double $coke_Can = 1.90;
+    public static double $coke_bottle = 2.50;
+    public static double $water = 1.00;
+    public static double $pepsi = 2.10;
+    public static double $monster_energy = 3.65;
+
+    //Side
+    public static double $chips = 4.95;
+    public static double $salad = 5.25;
+    public static double $onionRings = 3.65;
+    public static double $breadStix = 1.85;
+
+    
+}
